a3c078ecc0e481d2ac1fc2c1ef3855e1671ef2a6
Rabindra Amatya <amatyar@yahoo.com>
2017-01-18 17:35:51 -0500

    push again

---
+++ calculate.js
@@ -0,0 +1,21 @@
+function addBy() {
+  x = document.getElementById("firstNumber").value;
+  y = document.getElementById("secondNumber").value;
+  document.getElementById("result").innerHTML= x + y;
+}
+function subtractBy() {
+  x = document.getElementById("firstNumber").value;
+  y = document.getElementById("secondNumber").value;
+  document.getElementById("result").innerHTML= x - y;
+}
+function multiplyBy() {
+  x = document.getElementById("firstNumber").value;
+  y = document.getElementById("secondNumber").value;
+  document.getElementById("result").innerHTML= x * y;
+}
+function divideBy() {
+  x = document.getElementById("firstNumber").value;
+  y = document.getElementById("secondNumber").value;
+  document.getElementById("result").innerHTML= x / y;
+
+}
